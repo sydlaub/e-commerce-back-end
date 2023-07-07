defe9aba684cfaa54efc203f1b6cadc4e2dbf3e9
Sydney Laub <syd.laub18@gmail.com>
2023-07-07 13:58:37 -0400

    completed api tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -30,16 +30,33 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(req.body, { where: {id: req.params.id}});
+    res.status(200).json({ message: `${updateTag} tag updated!`});
+  } catch (err){
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({ where: {id: req.params.id}})
+  .then(tags => res.json({message: `${tags} tag deleted`}))
+  .catch(err => {
+    res.status(400).json(err);
+  })
 });
 
 module.exports = router;
