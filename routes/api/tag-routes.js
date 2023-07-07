fed2eb2c8490322ca42ecd7a370f77049955a80b
Sydney Laub <syd.laub18@gmail.com>
2023-07-07 13:33:47 -0400

    added api route to get all tags and api route to get a single tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,31 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  const allTags = await Tag.findAll({
+    // be sure to include its associated Product data
+    include: [{ model: Product, through: ProductTag }]
+  }).catch((err) => {
+    res.json(err);
+  });
+  res.json(allTags)
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try {
+    const singleTag = await Tag.findByPk(req.params.id, {
+      // be sure to include its associated Product data
+      include: [{model: Product, through: ProductTag}]
+    })
+    if (!singleTag) {
+      res.status(404).json({message: `No tag was found with that ID`})
+    }
+    res.status(200).json(singleTag);
+  } catch {
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
